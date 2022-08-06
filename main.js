9a2b48bca9951157c261e7539c9c0b5d95086680
lmj8033 <lmj8033@naver.com>
2022-08-07 02:44:13 +0900

    test

--- main.js
+++ main.js
@@ -17,30 +17,30 @@
 //     list[active].classList.add("on");
 // })
 
-const frame = document.querySelector("slider");
-const down = document.querySelector(".btnDown");
-const len = list.length;
-const deg = 360 / len;
-const names = ["planet1","planet2","planet3","planet4","planet5","planet6","planet7","planet8"]
+// const frame = document.querySelector("slider");
+// const down = document.querySelector(".btnDown");
+// const len = list.length;
+// const deg = 360 / len;
+// const names = ["planet1","planet2","planet3","planet4","planet5","planet6","planet7","planet8"]
 
-for(let i = 0; i < len; i++) {
-    list[i].style.transform = `rotate(${deg*i}deg) translateY(-100vh)`;
-}
+// for(let i = 0; i < len; i++) {
+//     list[i].style.transform = `rotate(${deg*i}deg) translateY(-100vh)`;
+// }
 
-let num = 0;
-let active = 0;
+// let num = 0;
+// let active = 0;
 
-down.addEventListener("click",function(e) {
-    frame.style.transfrom = `rotateY(${deg* ++num}deg)`;
+// down.addEventListener("click",function(e) {
+//     frame.style.transfrom = `rotateY(${deg* ++num}deg)`;
 
-    if (active === 0) {
-        active = len -1;
-    }else {
-        active--;
-    }
+//     if (active === 0) {
+//         active = len -1;
+//     }else {
+//         active--;
+//     }
 
-    for(let el of list) {
-        el.classlist.remove("on");
-    }
-    list[active].classlist.add("on");
-})
+//     for(let el of list) {
+//         el.classlist.remove("on");
+//     }
+//     list[active].classlist.add("on");
+// })
