40511924b9f946f7b98b3464d615545852e2d95f
lmj8033 <lmj8033@naver.com>
2022-08-08 18:58:06 +0900

    test

--- test.js
+++ test.js
@@ -14,4 +14,29 @@ window.addEventListener("scroll", function() {
         sunAnimate.classList.add("on");
         console.log(sunAnimate);
     }
+
+    if(scrollYpos > 830) {
+        const sunAnimate = document.querySelector(".spaceship");
+        sunAnimate.classList.add("on");
+        console.log(sunAnimate);
+    }
+
 })
+
+// function link() {
+//     setTimeout(function() {
+//         location.href = './mypage.html';
+//     }, 8000);
+// }
+
+const btn = document.querySelector(".earth");
+const img = document.querySelector(".img4>img");
+console.log(img);
+btn.addEventListener("click", function(){
+    img.setAttribute("transform", "scale(3.0)");
+    setTimeout(function() {
+        location.href = './mypage.html';
+    }, 8000);
+    
+
+});
